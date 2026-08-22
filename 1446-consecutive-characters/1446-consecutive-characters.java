63aa8c2683156cb39ef0b0e7139990c859d8d230
BALAJI R <balajicbe0@gmail.com>
2026-08-22 12:21:37 +0530

    Time: 1 ms (100%), Space: 43.4 MB (61.66%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.java
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -4,7 +4,7 @@ class Solution {
         int max=0;
         int count1=0;
         char c[]=s.toCharArray();
-        for(int i=0;i<c.length;i+=count1){
+        for(int i=0;i<c.length;i++){
             count=0;
             for(int j=i;j<c.length;j++){
                 if(c[i]==c[j])
@@ -14,7 +14,7 @@ class Solution {
             }
             if(max<count)
             max=count;
-            count1=count;
+            //count1=count;
         }
         return max;
     }
