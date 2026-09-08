220aba3a6e037c853b66c80c478b36e753800923
BALAJI R <balajicbe0@gmail.com>
2026-09-08 14:55:32 +0530

    Time: 0 ms (100%), Space: 41.9 MB (74.48%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int integerBreak(int n) {
+        if(n==2)
+        return 1;
+        if(n==3)
+        return 2;
+        int p=1;
+        while(n>4){
+            p*=3;
+            n-=3;
+        }
+        p=p*n;
+        return p;
+    }
+}
