16096460fce65e46f94f2797d56bd82405ff4854
BALAJI R <balajicbe0@gmail.com>
2026-09-14 19:24:07 +0530

    Time: 1 ms (98.44%), Space: 42.6 MB (55.07%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int digitFrequencyScore(int n) {
+        int[] freq = new int[10];
+        while (n > 0) {
+            int digit = n % 10;
+            freq[digit]++;
+            n = n / 10;
+        }
+        int ans = 0;
+        for (int i = 0; i < 10; i++) 
+            ans = ans + i * freq[i];
+        return ans;
+    }
+}
